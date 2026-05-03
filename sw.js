b6e8ab75c06b97a832aa9faeb224c8253a9a3647
putuuwiryaa-lab <putuuwiryaa@gmail.com>
2026-05-03 17:30:37 +0800

    Disable stale service worker caching

--- sw.js
+++ sw.js
@@ -1,63 +1,21 @@
-const CACHE_NAME = 'prediksi4d-pro-secure-1';
-const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css?v=secure-1',
-  '/premium-banner.css?v=1',
-  '/support.css?v=1',
-  '/engine.js?v=secure-1',
-  '/ui.js?v=secure-1',
-  '/topline-patch.js?v=secure-1',
-  '/support.js?v=1',
-  '/manifest.json',
-  '/icons/icon-192.svg',
-  '/icons/icon-512.svg'
-];
+const CACHE_NAME = 'prediksi4d-pro-secure-3';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
-  );
+  event.waitUntil(caches.delete(CACHE_NAME));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  const request = event.request;
-
-  if (request.method !== 'GET') return;
-
-  const url = new URL(request.url);
-  const isFunctionApi = url.pathname.includes('/functions/v1/get-markets');
-
-  if (isFunctionApi) {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(request, clone));
-          return response;
-        })
-        .catch(() => caches.match(request))
-    );
-    return;
-  }
+  if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    fetch(request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(request, clone));
-        return response;
-      })
-      .catch(() => caches.match(request))
+    fetch(event.request, { cache: 'no-store' })
   );
 });
